70451894cf050f306b52e59ad8d57b2833dfec40
Roberto Isla <robertoisla92@gmail.com>
2025-04-30 08:21:06 -0400

    Cambios de Theme

---
+++ src/components/styles/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette:{
+        primary: {
+            main: '#1976d2',
+        },
+        secondary:{
+            main: '#dc004e'
+        },
+    },
+    typography: '"Roboto", "Helvetica", "Arial", sans-serif',
+    
+});
+
+export default theme;
